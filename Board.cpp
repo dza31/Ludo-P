6c5ba04fb3c6d21b50c22ff29b44f1eeb50f775c
Usama848 <64732895+Usama848@users.noreply.github.com>
2020-05-05 20:07:42 +0500

    Updated Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,17 +1,57 @@
 #include "Board.h"
-#include"Team.h"
+#include "Team.h"
 
-int Board::NPcs()
-{
-	return 0;
+using namespace std;
+
+Board::Board(int bg, Point p, vector<Piece*> cp) : BG(bg), P(p), Safe(false) {
+	CP = cp;
 }
-bool Board::ValidPiece(Team T)
-{
+
+Board::Board(Player& P) {
+	BG = P.Clr;
+	Safe = true;
+}
+Board::Board() {
+
+}
+
+void Board::setPlayer(Player& P) {
+	BG = P.Clr;
+	Safe = true;
+}
+
+void Board::Draw() {
+
+}
+
+void Board::SetPoint(Point p) {
+	P = p;
+}
+
+void Board::MakeSafe() {
+	Safe = true;
+}
+
+int Board::NPcs() {
+	return CP.size();
+}
+
+bool Board::ValidPiece(Team T) {
+	for (int i = 0; i < CP.size(); i++) {
+		if (CP[i]->IsMyPiece(T) == true)
+			return true;
+	}
 	return false;
 }
 
-bool Board::ValidPiece(Player T)
-{
-
+bool Board::ValidPiece(Player P) {
+	for (int i = 0; i < CP.size(); i++) {
+		if (P.Clr == (*CP[i]).Clr)
+			return true;
+	}
 	return false;
+}
+
+bool Board::IsSafe() {
+	return Safe;
 }
